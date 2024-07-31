b27f515ac2569be304a16d43f8c0006c70d38570
Robert Coulson <43724396+Coulsyltd@users.noreply.github.com>
2024-07-31 15:46:10 +0100

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import tailwind from "@astrojs/tailwind";
 
 // import purgecss from "astro-purgecss";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()]
 });
