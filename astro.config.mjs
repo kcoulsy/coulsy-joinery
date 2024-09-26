43c1ae0c689006433dae01b3204e6e863a14ec51
Robert Coulson <43724396+Coulsyltd@users.noreply.github.com>
2024-09-26 09:38:31 +0100

    changes to type formated and truss roofs

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,9 +14,13 @@ export default defineConfig({
     changefreq: 'weekly',
     priority: 0.7,
     lastmod: new Date('2024-07-31'),
+    redirects: {
+      '/joinery-services/truss-roofs': {
+        status: 302,
+        destination: '/joinery-services/truss-roof-installation'
+      }
+    }
   }),
-    
-  ],
-  
 
+  ],
 });
