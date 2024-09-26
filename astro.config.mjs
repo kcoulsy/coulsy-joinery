aeaf54dd270fd66d2318bf2b3bcc330cb5181b0b
Robert Coulson <43724396+Coulsyltd@users.noreply.github.com>
2024-09-26 09:55:08 +0100

    mods

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig({
     changefreq: 'weekly',
     priority: 0.7,
     lastmod: new Date('2024-07-31'),
+    
     redirects: {
       '/joinery-services/truss-roofs': {
         status: 302,
--- src/constants/locations.ts
+++ src/constants/locations.ts
@@ -1,5 +1,4 @@
 export const LOCATIONS = [
-  "huddersfield",
   "wakefield",
   "castleford",
   "pontefract",
