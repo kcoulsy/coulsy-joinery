39ad32d9b4646522be8a2f2dbd4fde4f9d33b109
Kristian Coulson <kristiancoulson@gmail.com>
2024-09-27 09:07:22 +0100

    fix: build issue with redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://coulsyjoinery.co.uk/',
+  trailingSlash: 'never',
+  build: {
+    format: 'file',
+  },
   integrations: [tailwind(),
     sitemap({
     // configuration options
