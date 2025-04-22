0a2978f746bad610c306daaefaa0aa209e7f7b83
Robert Coulson <43724396+Coulsyltd@users.noreply.github.com>
2025-04-22 10:34:52 +0100

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,24 +5,24 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://coulsyjoinery.co.uk/',
-  trailingSlash: 'never',
+  site: "https://coulsyjoinery.co.uk/",
+  trailingSlash: "never",
   build: {
-    format: 'file',
+    format: "file",
   },
-  integrations: [tailwind(),
+  integrations: [
+    tailwind(),
     sitemap({
-    // configuration options
-    changefreq: 'weekly',
-    priority: 0.7,
-    lastmod: new Date('2024-07-31'),
-    redirects: {
-      '/joinery-services/truss-roofs': {
-        status: 302,
-        destination: '/joinery-services/truss-roof-installers'
-      }
-    }
-  }),
-
+      // configuration options
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date("2024-07-31"),
+      redirects: {
+        "/joinery-services/truss-roofs": {
+          status: 302,
+          destination: "/joinery-services/truss-roof-installers",
+        },
+      },
+    }),
   ],
 });
